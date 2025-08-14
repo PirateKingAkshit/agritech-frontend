b268dd6e601050ed32d2730c0e6adb224b986927
PirateKingAkshit <akshitkumar383@gmail.com>
2025-08-14 13:11:58 +0530

    Some Chnages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,29 @@
 import TerserPlugin from "terser-webpack-plugin";
 
-/** @type {import('next').NextConfig} */
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "127.0.0.1"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "5000", // or your dev port
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "5000", // or your dev port
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "agritech.cybermatrixsolutions.com",
+        pathname: "/**",
+      },
+    ],
   },
+
   // reactStrictMode: true,
 
   // swcMinify: true,
@@ -16,9 +34,7 @@ const nextConfig = {
   //       new TerserPlugin({
   //         terserOptions: {
   //           mangle: true,
-
   //           compress: true,
-
   //           output: {
   //             comments: false,
   //           },
@@ -26,7 +42,6 @@ const nextConfig = {
   //       })
   //     );
   //   }
-
   //   return config;
   // },
 
